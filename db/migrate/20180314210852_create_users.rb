d34f91c07539137ad6342b2c275fae6e5b296e6e
Julian <julianrosenthal@yahoo.com>
2020-12-28 12:11:16 -0800

    commit tree a17bf9c8c27ce1a3452f562e646b6be5d332b723

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,7 +1,13 @@
+# Creates/drops users table
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+    end
   end
-  
+
   def down
+    drop_table :users
   end
 end
